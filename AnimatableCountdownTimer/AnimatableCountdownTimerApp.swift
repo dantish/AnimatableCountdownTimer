a5407f615d36c09d318a5809514bcc262daff009
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-06 12:27:46 +0200

    Initial Commit

---
+++ AnimatableCountdownTimer/AnimatableCountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatableCountdownTimerApp.swift
+//  AnimatableCountdownTimer
+//
+//  Created by Daniel Tischenko on 06.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimatableCountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatableCountdownTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimatableCountdownTimer
+//
+//  Created by Daniel Tischenko on 06.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
