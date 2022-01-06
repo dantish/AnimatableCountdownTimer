6b757495ba7175ea8c91136a450a2c9c3c842989
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-06 12:37:42 +0200

    Create `CountdownTimer` file with the SwiftUI template

---
+++ AnimatableCountdownTimer/CountdownTimer.swift
@@ -0,0 +1,20 @@
+//
+//  CountdownTimer.swift
+//  AnimatableCountdownTimer
+//
+//  Created by Daniel Tischenko on 06.01.2022.
+//
+
+import SwiftUI
+
+struct CountdownTimer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CountdownTimer_Previews: PreviewProvider {
+    static var previews: some View {
+        CountdownTimer()
+    }
+}
