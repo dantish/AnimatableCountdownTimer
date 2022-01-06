f6f8b07383091a960c3b1762e1b70cbb5f41164b
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-06 14:57:02 +0200

    Implement a simple countdown timer without any styles

--- AnimatableCountdownTimer/CountdownTimer.swift
+++ AnimatableCountdownTimer/CountdownTimer.swift
@@ -8,13 +8,24 @@
 import SwiftUI
 
 struct CountdownTimer: View {
+    private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var timeRemaining = 100
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(String(timeRemaining))
+            .onReceive(timer) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                } else {
+                    timer.upstream.connect().cancel()
+                }
+            }
     }
 }
 
 struct CountdownTimer_Previews: PreviewProvider {
     static var previews: some View {
         CountdownTimer()
+            .previewLayout(.sizeThatFits)
     }
 }
