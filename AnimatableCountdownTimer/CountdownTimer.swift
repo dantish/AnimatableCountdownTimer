abfde0c39a0e0deaa5e0113a9e798ef7e6b45e73
Daniel Tischenko <daniil.tishenko.99@gmail.com>
2022-01-07 11:38:04 +0200

    Make the `CountdownTimer` view fixed width

--- AnimatableCountdownTimer/CountdownTimer.swift
+++ AnimatableCountdownTimer/CountdownTimer.swift
@@ -17,6 +17,7 @@ struct CountdownTimer: View {
             .foregroundColor(.white)
             .padding(.vertical, 4)
             .padding(.horizontal)
+            .frame(width: 100)
             .background(
                 Capsule()
                     .fill(Color.red)
